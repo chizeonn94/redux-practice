97936f9e54f8e84b8bbab908f89291850afa8f0f
unknown <jskim@atozsoft.kr>
2021-07-02 17:56:40 +0900

    completion of +,- buttons using redux!

--- practice/src/index.js
+++ practice/src/index.js
@@ -4,4 +4,33 @@ const minus = document.getElementById("minus");
 const plus = document.getElementById("plus");
 const number = document.getElementById("number");
 
-number.innerText=12
+let count = 0;
+number.innerText = count;
+const handleMinus = () => {
+ countStore.dispatch({type:'MINUS'})
+ console.log(count)
+}
+const handlePlus = () => {
+  countStore.dispatch({ type: 'ADD' })
+  console.log(count)
+}
+
+const countModifier = (count = 0, action) => {
+  switch (action.type) {
+    case 'ADD':
+      return count + 1;
+    case 'MINUS':
+      return count - 1;
+    default:
+      return count;
+    
+  }
+}
+const countStore = createStore(countModifier)
+minus.addEventListener('click', handleMinus)
+plus.addEventListener('click', handlePlus)
+
+const onChange = () => {
+  number.innerText = countStore.getState();
+}
+countStore.subscribe(onChange);
