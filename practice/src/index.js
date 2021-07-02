0cfd8d64dc1e3491d988e2e414d52f68ee9ab256
unknown <jskim@atozsoft.kr>
2021-07-02 16:46:28 +0900

    redux

--- practice/src/index.js
+++ practice/src/index.js
@@ -1,1 +1,7 @@
+import { createStore } from 'redux'
 
+const minus = document.getElementById("minus");
+const plus = document.getElementById("plus");
+const number = document.getElementById("number");
+
+number.innerText=12
